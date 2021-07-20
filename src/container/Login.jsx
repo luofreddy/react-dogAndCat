c5c6409c45069ae8de9ba039b194e378eab5f13a
Freddy <a0983330053@gmail.com>
2021-07-20 23:28:22 +0800

    feat router

---
+++ src/container/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Login = () => {
+  
+  
+  return (
+    <div>
+      <div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="false">使用FB登入</div>
+      
+    </div>
+  );
+};
+
+export default Login;
